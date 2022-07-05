728ff0649a4b02efdec4d8a46b91f3375f5185d8
Inon <pokemonxdloquendo@gmail.com>
2022-07-05 12:54:45 -0400

    cipher strings and ignores whitespaces

--- caesar.rb
+++ caesar.rb
@@ -1,14 +1,38 @@
+puts 'Inserte codigo a ser encriptado'
 input = gets.chomp
-#sumador = gets.to_i
+puts 'Inserte profundidad de encriptación'
+sumador = gets.to_i
 inputs = input.split('')
 array = []
 
 resul = inputs.each do |letras|
-    if letras != ' '
-        p numeros = letras.ord
-        p num_suma = numeros + 5
-        p num_cambio = num_suma.chr
-        p array.push(num_cambio)
+    numeros = letras.ord
+    if numeros != 32
+        num_suma = numeros + sumador
+        num_cambio = num_suma.chr
+        array.push(num_cambio)
+    end
+    if numeros == 32
+        array.push(numeros.chr)
     end
 end
-puts array.join
+
+final = array.join
+puts 'código cifrado: ' + final
+des_input = final.split('')
+des_array = []
+desci = des_input.each do |letras|
+    numeros = letras.ord
+    if numeros != 32
+         num_suma = numeros - 5
+         num_cambio = num_suma.chr
+         des_array.push(num_cambio)
+    end
+    if numeros == 32
+        des_array.push(numeros.chr)
+    end
+end
+puts 'codigo descifrado: ' + des_array.join
+
+
+
